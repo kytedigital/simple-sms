c7274fa1d0f13471e1dbcdc23f507139f6f0a755
Kai <kyle@kylewaters.co.uk>
2021-04-26 19:11:40 +1000

    X Frame Middleware

--- app/Http/Middleware/XFrameHeadersMiddleware.php
+++ app/Http/Middleware/XFrameHeadersMiddleware.php
@@ -30,7 +30,7 @@ class XFrameHeadersMiddleware
          *
          */
         $response = $next($request);
-        $response->headers->set('X-Frame-Options', 'SAMEORIGIN');
+        $response->headers->set('X-Frame-Options', '*.myshopify.com');
         return $response;
     }
 }
